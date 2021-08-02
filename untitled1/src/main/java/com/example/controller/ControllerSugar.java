b57dcd65f649414172e8aee8c8d07fbdde3e5b7a
Ngoc <nguyenhongngoc52@gmail.com>
2021-08-02 15:58:57 +0700

    SpringMVC demo

--- untitled1/src/main/java/com/example/controller/ControllerSugar.java
+++ untitled1/src/main/java/com/example/controller/ControllerSugar.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/SugarController")
 public class ControllerSugar {
     @RequestMapping("/sugar")
     @ResponseBody
     public String giveSugar(){
         return "OK...sugar";
+    }
+
+    @RequestMapping("/noSalt")
+    @ResponseBody
+    public String giveSlat()
+    {
+        return " khong co";
     }
 
 }
